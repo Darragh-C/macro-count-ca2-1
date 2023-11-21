e77043f2e8aa59e213c13421b92912eea461550e
Darragh Conneely <darraghc23@gmail.com>
2023-11-21 23:42:15 +0000

    list fragment proof of concept

--- app/src/main/java/org/wit/macrocount/MacroListFragment.kt
+++ app/src/main/java/org/wit/macrocount/MacroListFragment.kt
@@ -5,23 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import org.wit.macrocount.adapters.MacroCountAdapter
 import org.wit.macrocount.adapters.MacroCountListener
 import org.wit.macrocount.databinding.ActivityMacrocountListBinding
+import org.wit.macrocount.databinding.FragmentMacroListBinding
 import org.wit.macrocount.main.MainApp
 import org.wit.macrocount.models.MacroCountModel
 import org.wit.macrocount.models.UserRepo
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MacroListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MacroListFragment : Fragment(), MacroCountListener {
     // TODO: Rename and change types of parameters
     private lateinit var app: MainApp
@@ -31,40 +23,41 @@ class MacroListFragment : Fragment(), MacroCountListener {
     private var usersDailyMacroObjList = mutableListOf<MacroCountModel>()
     private var currentUserId: Long = 0
 
+    private var _fragBinding: FragmentMacroListBinding? = null
+    private val fragBinding get() = _fragBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
+
+        app = activity?.application as MainApp
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_macro_list, container, false)
+        _fragBinding = FragmentMacroListBinding.inflate(inflater, container, false)
+        val root = fragBinding.root
+        fragBinding.recyclerView.setLayoutManager(LinearLayoutManager(activity))
+        fragBinding.recyclerView.adapter = MacroCountAdapter(app.macroCounts.findAll(), this)
+
+        return root
+
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MacroListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance() =
             MacroListFragment().apply {
                 arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _fragBinding = null
     }
 
     override fun onMacroCountClick(macroCount: MacroCountModel) {
