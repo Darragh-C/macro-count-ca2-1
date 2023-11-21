f99f8acad9159a0ac171f919a6293184d0b23b24
Darragh Conneely <darraghc23@gmail.com>
2023-11-21 23:24:55 +0000

    created files required for test fragments

---
+++ app/src/main/java/org/wit/macrocount/activities/Home.kt
@@ -0,0 +1,12 @@
+package org.wit.macrocount.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.wit.macrocount.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
