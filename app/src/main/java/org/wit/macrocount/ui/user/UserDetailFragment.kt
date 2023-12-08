fa52ea9b954abea52dffc41b60b101758da61911
Darragh Conneely <darraghc23@gmail.com>
2023-12-08 23:04:05 +0000

    user detail to user nav

--- app/src/main/java/org/wit/macrocount/ui/user/UserDetailFragment.kt
+++ app/src/main/java/org/wit/macrocount/ui/user/UserDetailFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import org.wit.macrocount.R
 import org.wit.macrocount.databinding.FragmentUserDetailBinding
@@ -56,6 +57,10 @@ class UserDetailFragment : Fragment() {
         //Timber.i("observableMacro ${macro}")
 
 
+        fragBinding.editUser.setOnClickListener() {
+            val action = UserDetailFragmentDirections.actionUserDetailFragmentToUserFragment()
+            findNavController().navigate(action)
+        }
 
         return root
 
