864d9b25d84fbc3897134a57ab5b95c80703f7b5
Darragh Conneely <darraghc23@gmail.com>
2023-12-21 18:41:56 +0000

    basic bookmark colour toggle in adapter view holder

--- app/src/main/java/org/wit/macrocount/adapters/MacroCountAdapter.kt
+++ app/src/main/java/org/wit/macrocount/adapters/MacroCountAdapter.kt
@@ -1,8 +1,12 @@
 package org.wit.macrocount.adapters
 
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import org.wit.macrocount.R
 import org.wit.macrocount.databinding.CardMacrocountBinding
 import org.wit.macrocount.models.MacroCountModel
 
@@ -26,6 +30,7 @@ class MacroCountAdapter constructor(private var macroCounts: ArrayList<MacroCoun
     override fun onBindViewHolder(holder: MainHolder, position: Int) {
         val macroCount = macroCounts[holder.adapterPosition]
         holder.bind(macroCount, listener)
+
     }
 
     override fun getItemCount(): Int = macroCounts.size
@@ -43,21 +48,35 @@ class MacroCountAdapter constructor(private var macroCounts: ArrayList<MacroCoun
     class MainHolder(private val binding : CardMacrocountBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        private var isGrey = true
+
         fun bind(macroCount: MacroCountModel, listener: MacroCountListener) {
+
             binding.root.tag = macroCount
             binding.macroCounterTitle.text = macroCount.title
             binding.macroCountCalories.text = macroCount.calories + "Kj"
-            binding.macroCountProtein.text = "ptn:" + macroCount.protein + "g"
-            binding.macroCountCarbs.text = "crb:" + macroCount.carbs + "g"
-            binding.macroCountFat.text = "fat:" + macroCount.fat + "g"
-
+            binding.macroCountProtein.text = "P:" + macroCount.protein + "g"
+            binding.macroCountCarbs.text = "C:" + macroCount.carbs + "g"
+            binding.macroCountFat.text = "F:" + macroCount.fat + "g"
 
             binding.root.setOnClickListener {
                 listener.onMacroCountClick(macroCount)
             }
 
+            binding.bookmarkButton.setOnClickListener() {
+                toggleColor()
+            }
         }
+        private fun toggleColor() {
+            val newColor = if (isGrey) {
+                Color.parseColor("#0E76CC")
+            } else {
+                Color.LTGRAY
+            }
 
+            binding.bookmarkButton.setColorFilter(newColor)
+            isGrey = !isGrey
+        }
 
     }
 }
